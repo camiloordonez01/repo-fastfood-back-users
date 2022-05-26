8f875b29c36a809f98361636c0d4fac1756e92cb
Camilo Ordoñez <orarjuan@hotmail.com>
2022-05-26 13:34:31 -0500

    Add signUp Functionality Complete

---
+++ messages.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+    EMAIL_INVALID: 'El formato del correo electrónico es incorrecto',
+    PASSWORD_INVALID:
+        'La contraseña debe tener un mínimo de 8 caracteres y debe contener al menos un letra, un numero, una mayúscula y algunos de estos caracteres !@#$%^&*',
+    USER_EXISTING: 'El email ya esta en uso',
+    SUCCESS_SIGN_UP: 'El usuario se ha registrado exitosamente',
+})
