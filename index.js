9497105fc67ce99f336c07016c16e5a479d72f8e
Camilo Ordoñez <orarjuan@hotmail.com>
2022-05-26 16:52:19 -0500

    Add aws sdk and aws serverless

---
+++ index.js
@@ -0,0 +1,7 @@
+const awsServerlessExpress = require('aws-serverless-express')
+const { app } = require('./lib/infrastructure/webserver/server')
+
+const server = awsServerlessExpress.createServer(app)
+
+module.exports.handler = (event, context) =>
+    awsServerlessExpress.proxy(server, event, context)
